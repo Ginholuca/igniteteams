72fe5c5bd618b84a8608ddff60265177fa60f3e3
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-10-01 02:19:15 -0300

    Components

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import {
   useFonts,
@@ -6,7 +5,9 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 
-import { theme } from './theme'
+import { theme } from './src/theme'
+
+import { Loading } from '@components/Loading'
 
 import { Groups } from '@screens/Groups'
 
@@ -15,7 +16,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
 }
