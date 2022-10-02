84e260ae8d41bda5a539dff01cc7d01f1ce97e76
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-10-02 15:14:09 -0300

    HighLight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,14 @@
-import { Container } from './styles'
-
 import { Header } from '@components/Header'
+import { Highlight } from '@components/HighLight'
+
+import { Container } from './styles'
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   )
 }
