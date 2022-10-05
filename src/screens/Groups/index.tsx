0950e15c2cc3c953453996ad1409764c53ee6d0b
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-10-05 11:49:53 -0300

    Groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,8 @@
+import React, { useState } from 'react'
+
 import { Header } from '@components/Header'
 import { Highlight } from '@components/HighLight'
+import { GroupCard } from '@components/GroupCard'
 
 import { Container } from './styles'
 
