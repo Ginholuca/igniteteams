e0735f303045e8a1a5fe4b0d41a77ae4a02c38b9
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-10-05 12:28:53 -0300

    ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FlatList } from 'react-native'
 
 import { Header } from '@components/Header'
 import { Highlight } from '@components/HighLight'
@@ -7,11 +8,19 @@ import { GroupCard } from '@components/GroupCard'
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera do Bob'])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard key={item} title={item} />}
+      />
 
       <GroupCard title="Galera do Ignite" />
     </Container>
