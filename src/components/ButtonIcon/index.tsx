a6375f7d1363451576808141115c209fef3fa7b9
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-10-09 13:24:47 -0300

    Icon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,11 +1,17 @@
 import { TouchableOpacityProps } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
 
-import { Container, Icon } from './styles'
+import { ButtonIconTypeStyleProps, Container, Icon } from './styles'
 
-export function ButtonIcon({}: Props) {
+type Props = TouchableOpacityProps & {
+  icon: keyof typeof MaterialIcons.glyphMap
+  type?: ButtonIconTypeStyleProps
+}
+
+export function ButtonIcon({ icon, type = 'PRIMARY', ...rest }: Props) {
   return (
     <Container>
-      <Icon name="home" type="SECONDARY" />
+      <Icon name={icon} type={type} />
     </Container>
   )
 }
