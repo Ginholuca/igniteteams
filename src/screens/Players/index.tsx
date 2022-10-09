8cb8c611709f58d0a078595eed7544874a5b85bc
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-10-09 15:08:54 -0300

    FormPlayers

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,9 @@
-import { Container } from './styles'
+import { Container, Form } from './styles'
 
 import { Header } from '@components/Header'
+import { Input } from '@components/Input'
 import { Highlight } from '@components/HighLight'
+import { ButtonIcon } from '@components/ButtonIcon'
 
 export function Players() {
   return (
@@ -12,6 +14,12 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+      </Form>
+
+      <ButtonIcon icon="add" />
     </Container>
   )
 }
